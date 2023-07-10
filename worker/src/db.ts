5a2f39cba98ba9e8262b4becb368fc56b853b0f6
Cole <swiftyspiffy@gmail.com>
2023-07-10 01:32:36 -0700

    worker: initial database implementation and some request modeling and handling

---
+++ worker/src/db.ts
@@ -0,0 +1,116 @@
+import AWS, { DynamoDB } from "aws-sdk"
+
+export type DBPomodoro = {
+    TwitchStreamerId: string,
+    TwitchUserId: string,
+    PomodoroState: DBPomodoroState,
+}
+
+export enum DBPomodoroStatus {
+    Active,
+    Break
+}
+
+export enum DBPomodoroTaskStatus {
+    Awaiting,
+    InProgress,
+    Completed
+}
+
+export type DBPomodoroTask = {
+    Status: DBPomodoroTaskStatus,
+    Title: string,
+}
+
+export type DBPomodoroState = {
+    Status: DBPomodoroStatus,
+    EndsAt: number,
+    Tasks: DBPomodoroTask[],
+}
+
+export class Db {
+    private static REGION = "us-west-2"
+    private static TABLE = "Pomodoros";
+
+    private client: DynamoDB;
+
+    constructor(awsAccessKey: string, awsSecretKey: string) {
+        const creds = new AWS.Credentials(awsAccessKey, awsSecretKey);
+        this.client = new DynamoDB({ credentials: creds, region: Db.REGION })
+    }
+
+    /**
+     * Retrives all pomodoros associated with a specific streamer. Includes inactive pomodoros.
+     * @param streamerId Twitch User ID of streamer.
+     * @returns DBPomodoro
+     */
+    public GetStreamPomodoros(streamerId: string): [boolean, DBPomodoro[]] {
+        var params = {
+            ExpressionAttributeValues: {
+                ':streamer': {S: streamerId},
+            },
+            KeyConditionExpression: 'TwitchStreamerId = :streamer',
+            ProjectionExpression: 'TwitchStreamerId, TwitchUserId, PomodoroState',
+            TableName: Db.TABLE,
+        };
+
+        let channelPomodoros: DBPomodoro[];
+        this.client.query(params, function(err, data) {
+            if (err) {
+                console.log("Error", err);
+                return [false, null];
+            } else {
+                data.Items?.forEach(function(element, index, array) {
+                    channelPomodoros.push({
+                        TwitchStreamerId: streamerId,
+                        TwitchUserId: element.TwitchUserId.S!,
+                        PomodoroState: Db.parsePomodoroState(element.PomodoroState.S!)
+                    } as DBPomodoro)
+                });
+            }
+        });
+        return [true, channelPomodoros!];
+    }
+
+    /**
+     * Inserts or overwrites the user's existing pomodoro state.
+     * @param input DBPomodoro
+     */
+    public PutStreamPomodoro(input: DBPomodoro): boolean {
+        var params = {
+            TableName: Db.TABLE,
+            Item: {
+                'TwitchStreamerId': {S: input.TwitchStreamerId},
+                'TwitchUserId': {S: input.TwitchUserId},
+                'PomodoroState': {S: Db.pomodoroStateIntoJson(input.PomodoroState)},
+            }
+        }
+
+        this.client.putItem(params, function(err, data) {
+            if (err) {
+                console.log("Error", err);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    /**
+     * Helper function that converts a pomodoro state json string to DBPomodoroState.
+     * @param pomodoroState 
+     * @returns DBPomodoroState
+     */
+    private static parsePomodoroState(pomodoroState: string): DBPomodoroState {
+        // TODO: implement parsePomodoroState
+    }
+
+    /**
+     * Helper function that converts DBPomodoroState into json string.
+     * @param input 
+     * @returns string
+     */
+    private static pomodoroStateIntoJson(input: DBPomodoroState): string {
+        // TODO: implement pomodoroStateIntoJson
+    }
+
+}
--- worker/src/worker.ts
+++ worker/src/worker.ts
@@ -1,9 +1,13 @@
 import { Authorization, JWTPayload } from "./auth";
+import { DBPomodoro, Db } from "./db";
 
 export interface Env {
 	EXTENSION_SECRET: string
+	AWS_ACCESS_KEY: string
+	AWS_SECRET_KEY: string
 }
 
+const ENFORCE_JWT = false;
 const HEADER_EXTENSION_JWT = "X-Extension-Jwt";
 const PARAM_ACTION = "action";
 
@@ -31,30 +35,24 @@ export default {
 		const [isValidJWT, authorization, notValidJWTReason] = Authorization.VerifyJWT(request.headers.get(HEADER_EXTENSION_JWT)!, env.EXTENSION_SECRET);
 		if (!isValidJWT) {
 			console.log("invalid jwt: " + notValidJWTReason);
-			return this.ret(false, notValidJWTReason);
+			if(ENFORCE_JWT) {
+				return this.ret(false, notValidJWTReason);
+			}
 		}
+
+		// we can safely cast to non-null since we've already checked for error
+		const auth = authorization!
+
+		// create dynamo dao
+		const dbClient = new Db(env.AWS_ACCESS_KEY, env.AWS_SECRET_KEY);
 
 		// switch on action to determine what action we'll take
 		console.log("handling action: " + action);
 		switch(action) {
 			case "get_pomodoros":
-				// TODO: handle action.get_pomodoros
-				return this.ret(true, {
-					msg: "pomodors would be here",
-					authorization: authorization
-				});
+				return this.handleGetPomodoros(dbClient, auth);
 			case "put_pomodoro":
-				// TODO: handle action.put_pomodoro
-				return this.ret(true, {
-					msg: "confirmation of put pomodoro here",
-					authorization: authorization
-				});
-			case "update_pomodoro":
-				// TODO: handle action.update_pomodoro
-				return this.ret(true, {
-					msg: "confirmation of updated pomodoro here",
-					authorization: authorization
-				});
+				return this.handlePutPomodoro(dbClient, auth, request);
 			default:
 				return this.ret(false, {
 					msg: "unknown action: " + action,
@@ -63,6 +61,60 @@ export default {
 		}
 	},
 
+	/**
+	 * Helper method handles the action of putting a Twitch user's pomodoro into the database (`put_pomodoro`), which
+	 * also means overwriting an existing entry. This method will also handle dispathcing updates to the Twitch pubsub
+	 * to notify all extensions in a given channel of a change in a given pomodoro.
+	 * @param dbClient 
+	 * @param auth 
+	 * @returns 
+	 */
+	handlePutPomodoro(dbClient: Db, auth: JWTPayload, request: Request): Response {
+		// force the extension to send POST request
+		if(request.method != "POST") {
+			return this.ret(false, "request must be POST");
+		}
+
+		// force the extension to send form body
+		if(request.headers.get("content-type") != "form") {
+			return this.ret(false, "expected body content-type of form");
+		}
+
+		const putStreamPomodoroWasSuccessful = dbClient.PutStreamPomodoro({
+			TwitchStreamerId: auth.ChannelId,
+			TwitchUserId: auth.UserId,
+
+		} as DBPomodoro);
+		if(!putStreamPomodoroWasSuccessful) {
+			console.log("put stream pomodoro was unsuccessful");
+			return this.ret(false, "failed to put stream pomodoro");
+		}
+		// TODO: implement Twitch pubsub notification for modified pomodoro
+		return this.ret(true, {
+			msg: "successful",
+			authorization: auth
+		});
+	},
+
+	/**
+	 * Helper method handles the action of getting pomodoros (`get_pomodoros`), which returns active pomodoros for
+	 * a specified channel.
+	 * @param dbClient DynamoDB client wrapper.
+	 * @param auth Authorization data as provided by Twitch extension.
+	 * @returns Resposne
+	 */
+	handleGetPomodoros(dbClient: Db, auth: JWTPayload): Response {
+		const [getStreamPomodorosWasSuccessful, dbPomodoros] = dbClient.GetStreamPomodoros(auth.ChannelId);
+		if (!getStreamPomodorosWasSuccessful) {
+			console.log("get stream pomodoros was unsuccessful");
+			return this.ret(false, "failed to get stream pomodoros");
+		}
+		// TODO: filter out pomodoros that have since expired before returning them
+		return this.ret(true, {
+			msg: dbPomodoros,
+			authorization: auth
+		});
+	},
 
 	/**
 	 * Helper method that determines whether the request is valid and should be handled or not.
